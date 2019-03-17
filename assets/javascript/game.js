ee3e24045bc8bc5de734b3e13829ce2ec83814af
LeShawn Edouard <leshawnedouard92@gmail.com>
2019-03-17 15:10:44 -0400

    Attempting to enable the ajax library. Currently not recognized as a function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,15 +6,16 @@
   // Event listener for all button elements //
   $("button").on("click", function(event) {
 
-    var topics = $(this).attr([i]);
+
+    var topic = $(this).attr("data-topics");
 
     event.preventDefault();
 
-    var inputArtist = $("new-topic").val().trim();
+    // var inputArtist = $("new-topic").val().trim();
 
     // Constructing a URL to search Giphy for the topics in the array //
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      topics + "&api_key=HT501ZY2B0nY6zop9KdkAUZjCPQmpWJ0=10";
+      topic + "&api_key=HT501ZY2B0nY6zop9KdkAUZjCPQmpWJ0=10";
        
     // Ajax call that will return a promise //
     $.ajax({
@@ -23,20 +24,20 @@
     })
       // After the data comes back from the API //
       .then(function(response) {
-
-        var results = response.data
+          
+        var topics = response.data
 
         // For loop to help display topics as buttons //
         for (var i = 0; i < topics.length; i++) {
         
           // Will only take gifs that has appropriate rating //
-          if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+          if (topics[i].rating !== "r" && topics[i].rating !== "pg-13") {
 
             // Creating a div for the gifs //
             var gifDiv = $("<div>");
 
             // Storing the results topic's rating //
-            var rating = results[i].rating;
+            var rating = topics[i].rating;
 
             // Creating a paragraph tag with the result topic's rating //
             var p = $("<p>").text("Rating:" + rating);
@@ -46,7 +47,7 @@
 
             // Giving image tag a src attribute of a property pulled off the
             // result item
-            topicImage.attr("src", results[i].images.fixed_height.url);
+            topicImage.attr("src", topics[i].images.fixed_height.url);
 
             // Appending the paragraph and topicImage we created to the "gifDiv"
             // div we created
